c148f32bbf5c3c2802ef1bbb412b083a8ec81e3d
bbz <bbz@yahoo.com>
2010-10-10 12:28:53 -0700

    . add mtime preservation to rsync . create and serve zip before map

--- scripts/map.sh
+++ scripts/map.sh
@@ -1,5 +1,5 @@
 cd /home/blanu/minecraft/map/
-nice rsync -r --progress brandon@actlab.tv:minecraft/minecraft/world/ ./bigworld/
-nice ../cartograph-linux/cartograph -O=/home/synapthein/public_html/minecraft.png -l=day bigworld
+nice rsync -t -r --progress brandon@actlab.tv:minecraft/minecraft/world/ ./bigworld/
 rm /home/synapthein/public_html/minecraft-world.zip
 nice zip -9 -r /home/synapthein/public_html/minecraft-world.zip bigworld/
+nice ../cartograph-linux/cartograph -O=/home/synapthein/public_html/minecraft.png -l=day bigworld
