057b685be1620aa9be628adcc3cce3d85fced629
Brandon Wiley <brandon@blanu.net>
2010-10-11 12:02:55 -0500

    Removed -9 from zip options was it was taking too much CPU

--- scripts/map.sh
+++ scripts/map.sh
@@ -1,5 +1,5 @@
 cd /home/blanu/minecraft/map/
-nice rsync -t -r --progress brandon@actlab.tv:minecraft/minecraft/world/ ./bigworld/
+nice rsync -t -r brandon@actlab.tv:minecraft/minecraft/world/ ./bigworld/
 rm /home/synapthein/public_html/minecraft-world.zip
-nice zip -9 -r /home/synapthein/public_html/minecraft-world.zip bigworld/
+nice zip -r /home/synapthein/public_html/minecraft-world.zip bigworld/
 nice ../cartograph-linux/cartograph -O=/home/synapthein/public_html/minecraft.png -l=day bigworld
